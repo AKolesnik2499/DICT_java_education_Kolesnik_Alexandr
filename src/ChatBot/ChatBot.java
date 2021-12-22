a7d0c0570094e2ccf4679b3df12a96ceca5db2b4
Alexandr Kolesnik <o.o.koliesnik@student.khai.edu>
2021-12-22 18:31:50 +0300

    ChatBot

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,20 +1,14 @@
 package ChatBot;
-
 import java.time.LocalDate;
 import java.util.Scanner;
-
 public class ChatBot {
     public static void main(String[] args) {
-
         LocalDate localDate = LocalDate.now();
-
         System.out.println();
         System.out.println("Hello! My name is  Vasia");
         System.out.println("I was created in " + localDate);
         Scanner s11 = new Scanner(System.in);
         System.out.println("What a great name you have, " + s11.nextLine());
-
-
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
         ChatBot chatBot = new ChatBot();
         chatBot.calcAge(s11);
@@ -24,7 +18,6 @@ public class ChatBot {
         System.out.println("2. To decompose a program into several small subroutines");
         System.out.println("3. To determine the execution time of a program.");
         System.out.println("4. To interrupt the execution of a program.");
-
         while (true) {
             if (s11.next().contains("2")) {
                 System.out.println("Great, you right!\nGoodbye, have a nice day!");
@@ -55,5 +48,6 @@ public class ChatBot {
         System.out.println("Let's test your programming knowledge.");
     }
 }
+
 
 
